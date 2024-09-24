65d811e68e8c5b7bcc931f23a93a681df1fa366b
Al_Mahin <mmalmahin@gmail.com>
2024-09-24 21:59:32 +0600

    second

--- history.js
+++ history.js
@@ -72,22 +72,3 @@ document.querySelector('#my_modal_quota form button').addEventListener('click', 
         document.getElementById('quota-money').value = ''; // Clear input field
     }
 });
-
-// Show history or donation based on tab click
-document.getElementById('history-click').addEventListener('click', function() {
-    document.getElementById('main-history').classList.remove('hidden');
-    document.getElementById('main-donation').classList.add('hidden');
-    document.getElementById('history-click').classList.add('bg-[#B4F461]');
-    document.getElementById('history-click').classList.remove('btn-outline');
-    document.getElementById('donation-click').classList.add('btn-outline');
-    document.getElementById('donation-click').classList.remove('bg-[#B4F461]');
-});
-
-document.getElementById('donation-click').addEventListener('click', function() {
-    document.getElementById('main-donation').classList.remove('hidden');
-    document.getElementById('main-history').classList.add('hidden');
-    document.getElementById('donation-click').classList.add('bg-[#B4F461]');
-    document.getElementById('donation-click').classList.remove('btn-outline');
-    document.getElementById('history-click').classList.add('btn-outline');
-    document.getElementById('history-click').classList.remove('bg-[#B4F461]');
-});
--- toggle.js
+++ toggle.js
@@ -3,6 +3,7 @@ document.getElementById('history-click').addEventListener('click', function(){
     console.log('History Button Clicked');
     document.getElementById('main-history').classList.remove('hidden');
     document.getElementById('main-donation').classList.add('hidden');
+    document.getElementById('foot').classList.add('hidden');
     document.getElementById('history-click').classList.remove('btn-outline');
     document.getElementById('donation-click').classList.remove('bg-[#B4F461]');
     document.getElementById('donation-click').classList.add('btn-outline');
@@ -14,6 +15,7 @@ document.getElementById('donation-click').addEventListener('click', function(){
     console.log('Donation Button Clicked');
     document.getElementById('main-donation').classList.remove('hidden');
     document.getElementById('main-history').classList.add('hidden');
+    document.getElementById('foot').classList.remove('hidden');
     document.getElementById('history-click').classList.add('btn-outline');
     document.getElementById('donation-click').classList.add('bg-[#B4F461]');
     document.getElementById('donation-click').classList.remove('btn-outline');
